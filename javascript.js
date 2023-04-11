4b9897c2f6129e186f8bdd99e3213743c61f6ae0
vaishnavi-dev955 <vaishnavimandala1998@gmail.com>
2023-04-11 21:09:33 +0530

    Traffic Light

---
+++ javascript.js
@@ -0,0 +1,27 @@
+function onClickStop() {
+    document.getElementById("stopButton").style.backgroundColor = "#cf1124"
+    document.getElementById("stopLight").style.backgroundColor = "#cf1124"
+    document.getElementById("readyButton").style.backgroundColor = "#1f1d41"
+    document.getElementById("readyLight").style.backgroundColor = "#4b5069"
+    document.getElementById("goButton").style.backgroundColor = "#1f1d41"
+    document.getElementById("goLight").style.backgroundColor = "#4b5069"
+}
+
+function onClickReady() {
+    document.getElementById("readyButton").style.backgroundColor = "#f7c948"
+    document.getElementById("readyLight").style.backgroundColor = "#f7c948"
+    document.getElementById("goButton").style.backgroundColor = "#1f1d41"
+    document.getElementById("goLight").style.backgroundColor = "#4b5069"
+    document.getElementById("stopButton").style.backgroundColor = "#1f1d41"
+    document.getElementById("stopLight").style.backgroundColor = "#4b5069"
+}
+
+
+function onClickgo() {
+    document.getElementById("goButton").style.backgroundColor = "#199473"
+    document.getElementById("goLight").style.backgroundColor = "#199473"
+    document.getElementById("readyButton").style.backgroundColor = "#1f1d41"
+    document.getElementById("readyLight").style.backgroundColor = "#4b5069"
+    document.getElementById("stopButton").style.backgroundColor = "#1f1d41"
+    document.getElementById("stopLight").style.backgroundColor = "#4b5069"
+}
